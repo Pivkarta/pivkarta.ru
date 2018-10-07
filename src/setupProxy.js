1ac5c14fa6b4c69a5baf71f5ec2e49552972307a
Fi1osof <n.lanets@modxclub.ru>
2018-10-07 13:29:54 +0300

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,2 +1,21 @@
+const proxy = require('http-proxy-middleware');
 
-module.exports = require("@prisma-cms/front/src/setupProxy");
+
+module.exports = function (app) {
+
+  app.use(proxy('/api/', {
+    target: 'http://localhost:4100/',
+    ws: true,
+    pathRewrite: {
+      "^/api/": "/"
+    }
+  }));
+
+  app.use(proxy('/images/', {
+    target: 'http://localhost:4100/',
+    pathRewrite: {
+      "^/images/resized/([^/]+)/uploads/(.+)": "/images/$1/$2",
+    }
+  }));
+
+};
