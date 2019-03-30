71e595724b5dbd3f24cdf2454dd0c68f40737949
Fi1osof <n.lanets@modxclub.ru>
2019-03-30 15:30:52 +0300

    2.1.5

--- src/server/modules/city/index.mjs
+++ src/server/modules/city/index.mjs
@@ -31,21 +31,57 @@ const cities = async (source, args, ctx, info) => {
 
 const city = async (source, args, ctx, info) => {
 
-  return ctx.db.query.city({}, info);
+  return ctx.db.query.city(args, info);
 }
 
- 
+
+const createCity = async (source, args, ctx, info) => {
+
+  const {
+    currentUser,
+  } = ctx;
+
+  const {
+    sudo,
+  } = currentUser || {};
+
+  if(!sudo) {
+    throw new Error("Access denied");
+  }
+
+  return ctx.db.mutation.createCity(args, info);
+}
+
+
+const updateCity = async (source, args, ctx, info) => {
+
+  const {
+    currentUser,
+  } = ctx;
+
+  const {
+    sudo,
+  } = currentUser || {};
+
+  if(!sudo) {
+    throw new Error("Access denied");
+  }
+
+  return ctx.db.mutation.updateCity(args, info);
+}
+
+
 
 
 export default class CityModule extends PrismaModule {
 
-  
-  constructor(props){
-    
+
+  constructor(props) {
+
     super(props);
 
     this.objectType = "City";
-    
+
   }
 
   getResolvers() {
@@ -53,19 +89,21 @@ export default class CityModule extends PrismaModule {
 
     const resolvers = super.getResolvers();
 
-    Object.assign(resolvers.Query, { 
+    Object.assign(resolvers.Query, {
       cities,
       city,
     });
 
 
-    Object.assign(resolvers.Mutation, { 
+    Object.assign(resolvers.Mutation, {
+      createCity,
+      updateCity,
     });
 
 
 
 
-    Object.assign(resolvers, { 
+    Object.assign(resolvers, {
     });
 
 
